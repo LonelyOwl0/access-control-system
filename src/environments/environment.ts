23dff07eb5463d8da4752c15ea60cc9b58595b8c
medfo <medfouad2000@gmail.com>
2023-10-02 09:10:33 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'access-control-system-f99c7',
+    appId: '1:297127852419:web:67f9437321072f5ca2deb6',
+    databaseURL: 'https://access-control-system-f99c7-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'access-control-system-f99c7.appspot.com',
+    apiKey: 'AIzaSyB_QfO6xaZavNWb-Z0IxjajrLMFbnrFugk',
+    authDomain: 'access-control-system-f99c7.firebaseapp.com',
+    messagingSenderId: '297127852419',
+  },
+};
